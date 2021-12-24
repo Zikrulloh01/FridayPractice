01c3670e6058cbb72adff14abc1e85f6cea11ade
Zikrulloh Islomov <zikrilloislomov@gmail.com>
2021-12-24 15:37:43 +0000

    a new commit

--- src/test/java/Tc_002.java
+++ src/test/java/Tc_002.java
@@ -2,6 +2,7 @@ public class Tc_002 {
     public static void main(String[] args) {
 
         System.out.println("New branch has been created");
+        System.out.println("Another line added");
 
     }
 }
